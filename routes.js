5f01a62c142b989d6758712641423a6992b9810b
Lee Morgan <me@leemorgan.io>
2021-12-03 16:36:42 +0300

    Create the register page and style it.

--- routes.js
+++ routes.js
@@ -3,5 +3,9 @@ const home = require("./controllers/home.js");
 module.exports = (app)=>{
     let views = `${__dirname}/views/`;
 
+    //USERS
+    app.get("/user/register", (req, res)=>res.sendFile(`${views}/user/register.html`));
+
+    //OTHER
     app.get("/", (req, res)=>res.sendFile(`${views}/landingPage.html`));
 }
