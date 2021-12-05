73319e5bc39a02c7d80f06cead9ee87709ea13db
Lee Morgan <me@leemorgan.io>
2021-12-05 13:11:48 +0300

    Create admin registration page.

--- routes.js
+++ routes.js
@@ -4,15 +4,13 @@ const other = require("./controllers/other.js");
 const mid = require("./middleware.js");
 
 module.exports = (app)=>{
-    let views = `${__dirname}/views/`;
-
     //USERS
-    app.get("/user/register", mid.banner, (req, res)=>res.render(`${views}/user/register.ejs`, {banner: res.locals.banner}));
-    app.get("/user/login", mid.banner, (req, res)=>res.render(`${views}/user/login.ejs`, {banner: res.locals.banner}));
-    app.get("/user/verify/notify", mid.banner, (req, res)=>res.render(`${views}/user/verify.ejs`, {banner: res.locals.banner}));
-    app.get("/user/password/email", mid.banner,(req, res)=>res.render(`${views}/user/passwordEmail.ejs`, {banner: res.locals.banner}));
-    app.get("/user/password/:id/:session", mid.banner, (req, res)=>res.render(`${views}/user/password.ejs`, {banner: res.locals.banner}));
-    app.get("/user/dashboard", mid.auth("user"), mid.banner, (req, res)=>res.render(`${views}/user/dashboard.ejs`, {banner: res.locals.banner}));
+    app.get("/user/register", mid.banner, (req, res)=>res.render("user/register.ejs", {banner: res.locals.banner}));
+    app.get("/user/login", mid.banner, (req, res)=>res.render("user/login.ejs", {banner: res.locals.banner}));
+    app.get("/user/verify/notify", mid.banner, (req, res)=>res.render("user/verify.ejs", {banner: res.locals.banner}));
+    app.get("/user/password/email", mid.banner,(req, res)=>res.render("user/passwordEmail.ejs", {banner: res.locals.banner}));
+    app.get("/user/password/:id/:session", mid.banner, (req, res)=>res.render("user/password.ejs", {banner: res.locals.banner}));
+    app.get("/user/dashboard", mid.auth("user"), mid.banner, (req, res)=>res.render("user/dashboard.ejs", {banner: res.locals.banner}));
 
     app.post("/user/register", user.create);
     app.post("/user/login", user.login);
@@ -22,7 +20,7 @@ module.exports = (app)=>{
     app.post("/user/password/reset", user.passwordReset);
 
     //ADMIN
-    app.get("/admin/register", admin.create);
+    app.get("/admin/register", mid.banner, (req, res)=>res.render("admin/register.ejs", {banner: res.locals.banner}));
 
     //OTHER
     app.get("/", mid.banner, (req, res)=>res.render(`${views}/landingPage.ejs`, {banner: res.locals.banner}));
