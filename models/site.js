058710562ed973324fea97a1b7c7a1d94fb86e52
Lee Morgan <me@leemorgan.io>
2021-12-17 13:49:39 +0300

    Create the data model for sites.

---
+++ models/site.js
@@ -0,0 +1,99 @@
+const mongoose = require("mongoose");
+
+const SiteSchema = new mongoose.Schema({
+    projectName: String,
+    projectNumber: String,
+    phone: String,
+    address: {
+        number: String,
+        street: String,
+        city: String,
+        county: String,
+        state: String,
+        zip: String,
+        country: String,
+        full: String,
+        lat: Number,
+        long: Number
+    },
+    propertyType: String,
+    surveryDate: Date,
+    squareFootage: {
+        patio: Number,
+        diningRoom: Number,
+        total: Number,
+        verificationMethod: String
+    },
+    parkingSpaces: Number,
+    existingWC: {
+        men: {
+            occupancyCount: Number,
+            lavatories: Number,
+            toilets: Number,
+            urinals: Number
+        },
+        women: {
+            occupancyCount: Number,
+            lavatories: Number,
+            toilets: Number,
+            urinals: Number
+        },
+        unisex: {
+            occupancyCount: Number,
+            lavatories: Number,
+            toilets: Number,
+            urinals: Number
+        },
+        employee: {
+            occupancyCount: Number,
+            lavatories: Number,
+            toilets: Number,
+            urinals: Number
+        },
+        handDryers: Number,
+        faucets: Number,
+        concernsADA: [String]
+    },
+    contacts: [{
+        department: String,
+        title: String,
+        address: {
+            number: String,
+            street: String,
+            city: String,
+            county: String,
+            state: String,
+            zip: String,
+            country: String,
+            full: String,
+            lat: Number,
+            long: Number
+        },
+        phone: String,
+        email: String
+    }],
+    lastUpdated: Date,
+    codes: {
+        building: [String],
+        fire: [String],
+        pumbing: [String],
+        electrical: [String],
+        mechanical: [String],
+        energy: [String],
+        accessibility: [String],
+        other: [String]
+    },
+    requirementsADA: [String],
+    requirementsWC: [String],
+    applicablePermits: [{
+        permit: String,
+        sequence: String,
+        requirements: [String],
+        applicationFees: Number,
+        permitFees: Number,
+        reviewTime: Number
+    }],
+    thirdPartyReviews: [String]
+});
+
+module.exports = mongoose.model("site", SiteSchema);
